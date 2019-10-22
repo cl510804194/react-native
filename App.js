0e137a598f854ad5c5a6d396e61b59e79ea3db64
chengliang <510804194@qq.com>
2019-10-22 23:07:23 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>your mother facker?</Text>
+      <Text>你妈死了</Text>
+      <Text>你妈死了</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
